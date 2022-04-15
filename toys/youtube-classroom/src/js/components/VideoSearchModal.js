69474d28920c1249e11a7f9ec96709409249582c
Suyoung <jsy19970512@naver.com>
2022-04-15 17:26:19 +0900

    feat: Toast 메시지 기능 추가 - 비디오 저장 / 삭제 시 여부를 확인할 수 있도록 Toast Message 출력

--- toys/youtube-classroom/src/js/components/VideoSearchModal.js
+++ toys/youtube-classroom/src/js/components/VideoSearchModal.js
@@ -38,10 +38,21 @@ const VideoSearchModal = class extends Component {
           </section>
       </div>
     </dialog>
+    <dialog class="snackbar snackbar--success-save hide">
+    <div class="success-icon">
+      <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M21 10.0857V11.0057C20.9988 13.1621 20.3005 15.2604 19.0093 16.9875C17.7182 18.7147 15.9033 19.9782 13.8354 20.5896C11.7674 21.201 9.55726 21.1276 7.53447 20.3803C5.51168 19.633 3.78465 18.2518 2.61096 16.4428C1.43727 14.6338 0.879791 12.4938 1.02168 10.342C1.16356 8.19029 1.99721 6.14205 3.39828 4.5028C4.79935 2.86354 6.69279 1.72111 8.79619 1.24587C10.8996 0.770634 13.1003 0.988061 15.07 1.86572" stroke="#48B16E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M21 3.00574L11 13.0157L8 10.0157" stroke="#48B16E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>  
+    </div>
+    <div class="columnspace(5)"></div>
+    <p class="snackbar--message"></p>
+    </dialog>
     `;
 
     this.$target.insertAdjacentHTML('beforeend', template);
     this.$modalContainer = $('.modal-container');
+    this.$snackbar = $('.snackbar');
   }
 
   bindEvent() {
@@ -85,6 +96,8 @@ const VideoSearchModal = class extends Component {
     if (isSaveVideoBtn) {
       this.dispatchCustomEvent($('#app'), 'saveVideo', videoId);
       // TODO: 더 이상 저장할 수 없는 경우 예외처리
+      $('.snackbar--message').textContent = '성공적으로 저장되었습니다!';
+      this.renderSnackbar();
       target.disabled = true;
       target.nextElementSibling.disabled = false;
       return;
@@ -92,9 +105,19 @@ const VideoSearchModal = class extends Component {
 
     if (isUnsaveVideoBtn) {
       this.dispatchCustomEvent($('#app'), 'unsaveVideo', videoId);
+      $('.snackbar--message').textContent = '성공적으로 삭제되었습니다!';
+      this.renderSnackbar();
       target.disabled = true;
       target.previousElementSibling.disabled = false;
     }
+  }
+
+  renderSnackbar() {
+    this.$snackbar.classList.toggle('hide');
+
+    setTimeout(() => {
+      this.$snackbar.classList.toggle('hide');
+    }, 2000);
   }
 
   openModal() {
