f61423b1b792937faf01dd8665576036ae99e6df
Suyoung <jsy19970512@naver.com>
2022-04-25 16:21:58 +0900

    chore: ESLint & Prettier 설치 및 설정 파일 생성

---
+++ vendingmachine-ts/.prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	singleQuote: true,
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: false,
+	semi: true,
+	quoteProps: 'as-needed',
+	trailingComma: 'es5',
+	arrowParens: 'always',
+	endOfLine: 'lf',
+	bracketSpacing: true,
+	requirePragma: false,
+	insertPragma: false,
+	proseWrap: 'preserve'
+};
